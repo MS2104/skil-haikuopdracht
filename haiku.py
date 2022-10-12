5de6aa6e79a62a6a2277b0f22469d8426e5ec8d4
Ryan Schorel <34758@ma-web.nl>
2022-10-12 11:59:48 +0200

    Haiku1

--- haiku.py
+++ haiku.py
@@ -5,6 +5,10 @@ def Haiku1():
     print("- Natsume Soseki")
 
 
+def Haiku2():
+    print("I kill an ant and realize my three children have been watching.")
+    print("- Kato Shuson")
+
 #laat de code hieronder met rust
 Haiku1()
 Haiku2()
