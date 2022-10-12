15c614ecb27c9f2ad68c60a3aa49eaf982e10315
MS2104 <31002@ma-web.nl>
2022-10-12 11:54:34 +0200

    haiku 1 ingevuld

--- haiku.py
+++ haiku.py
@@ -1,6 +1,8 @@
 #plak hier de haiku functions onder elkaar
 
-
+def Haiku1():
+    print("Over the wintry forest, winds howl in rage with no leaves to blow.")
+    print("- Natsume Soseki")
 
 
 #laat de code hieronder met rust
